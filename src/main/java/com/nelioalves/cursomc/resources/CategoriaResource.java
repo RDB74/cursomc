1bbbdeefed4d9dd8f56ae86ba7f3739e1ab6de90
RDB74 <ricardoedayse@gmail.com>
2020-11-20 12:57:47 -0300

    Ajustes no endpoint /Categorias / {id}

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -24,9 +24,6 @@ public class CategoriaResource {
 	public ResponseEntity<?> find(@PathVariable Integer id){
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok(obj);
-		
-		
-		
 	}
 
 }
